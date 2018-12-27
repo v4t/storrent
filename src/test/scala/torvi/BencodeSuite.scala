1a1211569444a5e0de782eab1669e21192cbded9
torvi <torvi>
2018-12-27 16:30:16 +0200

    Add bencode integer parsing

--- src/test/scala/torvi/BencodeSuite.scala
+++ src/test/scala/torvi/BencodeSuite.scala
@@ -13,4 +13,14 @@ class BencodeSuite extends FunSuite {
     val value = "0:4:test"
     assert(Bencode.decode(value.toStream) == List(BencodeStringValue(""), BencodeStringValue("test")))
   }
+
+  test("Bencode decodes integer literals") {
+    val value = "i2414e"
+    assert(Bencode.decode(value.toStream) == List(BencodeIntValue(2414)))
+  }
+
+  test("Bencode decodes negative integer literals") {
+    val value = "i-500e"
+    assert(Bencode.decode(value.toStream) == List(BencodeIntValue(-500)))
+  }
 }
