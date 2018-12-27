8a1bab23791edab0f3e663b82fed6cc306659342
v4t <39200058+v4t@users.noreply.github.com>
2018-12-27 15:49:34 +0200

    Parse bencode strings

--- src/main/scala/torvi/Bencode.scala
+++ src/main/scala/torvi/Bencode.scala
@@ -1,12 +1,25 @@
 package torvi
 
+case class BencodeParseException(str: String) extends Exception
+
 trait BencodeValue
 
 case class BencodeStringValue(value: String) extends BencodeValue
 
 object Bencode {
 
-  def decode(stream: Stream[Char]) : List[BencodeValue] = ???
+  def decode(stream: Stream[Char]) : List[BencodeValue] = decodeRec(stream, Nil).reverse
+
+  private def decodeRec(stream: Stream[Char], cur: List[BencodeValue]) : List[BencodeValue] = {
+    if(stream.isEmpty) cur
+    else stream.head match {
+      case c if c.isDigit =>
+        val strLength = stream.takeWhile(i => ':' != i).mkString
+        val (str, next) = stream.drop(strLength.length + 1).splitAt(strLength.toInt)
+        decodeRec(next, BencodeStringValue(str.mkString) :: cur)
+      case _ => throw BencodeParseException("Could not parse identifier: " + stream.head)
+    }
+  }
 
   def encode(values: List[BencodeValue]): Unit = ???
 }
