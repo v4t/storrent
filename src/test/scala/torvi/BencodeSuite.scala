60144915e49922a26cb2c68141d8afdee1830b04
torvi <torvi>
2018-12-28 17:12:07 +0200

    Opt to use parser combinators for bencode rather than parsin by hand

--- src/test/scala/torvi/BencodeSuite.scala
+++ src/test/scala/torvi/BencodeSuite.scala
@@ -6,21 +6,21 @@ class BencodeSuite extends FunSuite {
 
   test("Bencode decodes string literals") {
     val value = "4:test"
-    assert(Bencode.decode(value.toStream) == List(BencodeStringValue("test")))
+    assert(BencodeParser.parse(value) == List(BencodeStringValue("test")))
   }
 
   test("Bencode decodes empty and nonempty string literals") {
     val value = "0:4:test"
-    assert(Bencode.decode(value.toStream) == List(BencodeStringValue(""), BencodeStringValue("test")))
+    assert(BencodeParser.parse(value) == List(BencodeStringValue(""), BencodeStringValue("test")))
   }
 
   test("Bencode decodes integer literals") {
     val value = "i2414e"
-    assert(Bencode.decode(value.toStream) == List(BencodeIntValue(2414)))
+    assert(BencodeParser.parse(value) == List(BencodeIntValue(2414)))
   }
 
   test("Bencode decodes negative integer literals") {
     val value = "i-500e"
-    assert(Bencode.decode(value.toStream) == List(BencodeIntValue(-500)))
+    assert(BencodeParser.parse(value) == List(BencodeIntValue(-500)))
   }
 }
