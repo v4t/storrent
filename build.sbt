0a7358e5442cbcbf02552ea8dc50670cdedc699c
v4t <39200058+v4t@users.noreply.github.com>
2018-12-28 17:12:07 +0200

    Opt to use parser combinators for bencode rather than parsin by hand

--- build.sbt
+++ build.sbt
@@ -8,5 +8,5 @@ libraryDependencies ++= Seq(
   "com.typesafe.akka" %% "akka-actor" % "2.5.19",
   "com.typesafe.akka" %% "akka-testkit" % "2.5.19" % Test
 )
-
+libraryDependencies += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.1.1"
 libraryDependencies += "org.scalatest" % "scalatest_2.12" % "3.0.5" % "test"
--- src/main/scala/torvi/Bencode.scala
+++ src/main/scala/torvi/Bencode.scala
@@ -5,25 +5,35 @@ case class BencodeParseException(str: String) extends Exception
 trait BencodeValue
 
 case class BencodeStringValue(value: String) extends BencodeValue
+
 case class BencodeIntValue(value: Long) extends BencodeValue
 
-object Bencode {
+import scala.util.parsing.combinator._
 
-  def decode(stream: Stream[Char]) : List[BencodeValue] = decodeRec(stream, Nil).reverse
+object BencodeParser extends RegexParsers {
 
-  private def decodeRec(stream: Stream[Char], cur: List[BencodeValue]) : List[BencodeValue] = {
-    if(stream.isEmpty) cur
-    else stream.head match {
-      case c if c.isDigit =>
-        val strLength = stream.takeWhile(i => ':' != i).mkString
-        val (str, next) = stream.drop(strLength.length + 1).splitAt(strLength.toInt)
-        decodeRec(next, BencodeStringValue(str.mkString) :: cur)
-      case c if c == 'i' =>
-        val number = stream.tail.takeWhile(i => 'e' != i).mkString
-        decodeRec(stream.drop(number.length + 2), BencodeIntValue(number.toInt) :: cur)
-      case _ => throw BencodeParseException("Could not parse identifier: " + stream.head)
+  override def skipWhitespace: Boolean = false
+
+  def parse(source: String) = {
+    parseAll(bencode, source) match {
+      case Success(matched, _) => matched
+      case Failure(msg, _) => throw new BencodeParseException(msg)
+      case Error(msg, _) => throw new BencodeParseException(msg)
     }
   }
 
-  def encode(values: List[BencodeValue]): Unit = ???
+  def bencode: Parser[List[BencodeValue]] = rep(value)
+
+  def value: Parser[BencodeValue] = elem
+
+  def elem: Parser[BencodeValue] = emptyString ||| string ||| int
+
+  def emptyString: Parser[BencodeValue] = "0:" ^^ (_ => BencodeStringValue(""))
+
+  def string: Parser[BencodeValue] = ("""[1-9]\d*""".r <~ ":") >> { strLength =>
+    repN(strLength.toInt, ".|\n".r) ^^ (i => BencodeStringValue(i.mkString))
+  }
+
+  def int: Parser[BencodeValue] = "i" ~> """(0|\-?[1-9]\d*)""".r <~ "e" ^^ (i => BencodeIntValue(i.toLong))
+
 }
