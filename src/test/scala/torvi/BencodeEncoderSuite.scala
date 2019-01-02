11ea8428c5df91ea0e9305f04e6c0aa87b9152fb
torvi <torvi>
2019-01-02 23:01:19 +0200

    Add couple more tests for string and integer value bencoding

--- src/test/scala/torvi/BencodeEncoderSuite.scala
+++ src/test/scala/torvi/BencodeEncoderSuite.scala
@@ -4,10 +4,24 @@ import org.scalatest.FunSuite
 
 class BencodeEncoderSuite extends FunSuite {
 
-  test("BencodeValueString can be encoded into string literal") {
+  test("BencodeStringValue can be encoded into bencode string") {
     val value = List(BencodeStringValue("test"))
     assert(BencodeEncoder.encode(value) == "4:test")
   }
 
+  test("Both empty and nonempty BencodeStringValues can be encoded into bencode strings") {
+    val value = List(BencodeStringValue(""), BencodeStringValue("test"))
+    assert(BencodeEncoder.encode(value) == "0:4:test")
+  }
+
+  test("BencodeIntValue can be encoded into bencode string") {
+    val value = List(BencodeIntValue(2414))
+    assert(BencodeEncoder.encode(value) == "i2414e")
+  }
+
+  test("Negative BencodeIntValues can be encoded into bencode string") {
+    val value = List(BencodeIntValue(-500))
+    assert(BencodeEncoder.encode(value) == "i-500e")
+  }
 }
 
