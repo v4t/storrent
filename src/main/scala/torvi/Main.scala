4923dc173cb38839b5a0e38c5b69c6010ac2bb0c
torvi <torvi>
2019-01-13 18:53:43 +0200

    Start working on the main program

--- src/main/scala/torvi/Main.scala
+++ src/main/scala/torvi/Main.scala
@@ -1,5 +1,14 @@
 package torvi
+import akka.actor.{Props, ActorSystem}
 
 class Main {
+  def main(args: Array[String]) {
+    if (args.isEmpty) {
+      println("Usage: storrent [torrent file]")
+      System.exit(0)
+    }
 
+    val system = ActorSystem("scala-torrent")
+    
+  }
 }
