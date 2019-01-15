aae45df3c65bd535c34ec21e8123364623142804
v4t <39200058+v4t@users.noreply.github.com>
2019-01-16 01:29:51 +0200

    Add .torrent file processing, fix some encoding related issues

--- src/main/scala/torvi/STorrent.scala
+++ src/main/scala/torvi/STorrent.scala
@@ -1,13 +1,23 @@
 package torvi
 
 import akka.actor.Actor
+import torvi.Bencode.BencodeParser
+
+import scala.io.{Codec, Source}
 
 case class StartDownload(torrentFile: String)
 
 class STorrent extends Actor {
   def receive = {
-    case StartDownload(f) =>
-      println(s"$f")
+    case StartDownload(torrentFile) => {
+      println(torrentFile)
+      val source = Source.fromFile(torrentFile)(Codec.ISO8859)
+      val contents = source.mkString
+      source.close
+      val bencodeValues = BencodeParser.parse(contents)
+      println(bencodeValues)
+    }
+
   }
 
 }
