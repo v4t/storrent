265f964d28a327d3a467b54a65d3f8810facc2e9
v4t <39200058+v4t@users.noreply.github.com>
2019-04-24 20:31:33 +0300

    Refactor metainfo dictionary

--- src/main/scala/storrent/metainfo/MetaInfoDictionary.scala
+++ src/main/scala/storrent/metainfo/MetaInfoDictionary.scala
@@ -11,28 +11,40 @@ case class MetaInfoDictionary(
 )
 
 object MetaInfoDictionary {
-  def fromBencode(bencodeDict: Map[BencodeString, BencodeValue]): MetaInfoDictionary = {
-    val pieceLength = bencodeDict.get(BencodeString("piece length")) match {
+  def fromBencode(bencodeDict: Map[BencodeString, BencodeValue]): MetaInfoDictionary =
+    MetaInfoDictionary(
+      pieceLength = pieceLength(bencodeDict),
+      pieces = pieces(bencodeDict),
+      noExternalPeerSource = noExternalPeerSource(bencodeDict),
+      name = name(bencodeDict),
+      files = FileInfo.fromBencode(bencodeDict)
+    )
+
+  private def pieceLength(dict: Map[BencodeString, BencodeValue]): Long =
+    dict.get(BencodeString("piece length")) match {
       case Some(BencodeInt(value)) => value
+      case Some(_) => throw MetaInfoException("Piece length field must be an integer")
+      case None => throw MetaInfoException("Piece length field is required")
     }
-    val pieces = bencodeDict.get(BencodeString("pieces")) match {
+
+  private def pieces(dict: Map[BencodeString, BencodeValue]): String =
+    dict.get(BencodeString("pieces")) match {
       case Some(BencodeString(value)) => value
+      case Some(_) => throw MetaInfoException("Pieces field must be a string")
+      case None => throw MetaInfoException("Pieces field is required")
     }
-    val noExternalPeerSource = bencodeDict.get(BencodeString("private")) match {
+
+  private def noExternalPeerSource(dict: Map[BencodeString, BencodeValue]): Option[Long] =
+    dict.get(BencodeString("private")) match {
       case Some(BencodeInt(value)) => Some(value)
       case _ => None
     }
-    val name = bencodeDict.get(BencodeString("name")) match {
-      case Some(BencodeString(value)) => value
-    }
-    val files = FileInfo.fromBencode(bencodeDict)
 
-    MetaInfoDictionary(
-      pieceLength = pieceLength,
-      pieces = pieces,
-      noExternalPeerSource = noExternalPeerSource,
-      name = name,
-      files = files
-    )
-  }
+  private def name(dict: Map[BencodeString, BencodeValue]): String =
+    dict.get(BencodeString("name")) match {
+      case Some(BencodeString(value)) => value
+      case Some(_) => throw MetaInfoException("Name field must be a string")
+      case None => throw MetaInfoException("Name field is required")
+    }
+
 }
