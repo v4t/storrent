386e7d1a29a47e812d8cb4e603c898daae84cf55
v4t <39200058+v4t@users.noreply.github.com>
2020-06-16 23:57:04 +0300

    Fix parsing invalid length handshakes

--- src/main/scala/storrent/peers/Handshake.scala
+++ src/main/scala/storrent/peers/Handshake.scala
@@ -16,10 +16,10 @@ object Handshake {
     )
 
   def decode(bytes: Array[Byte]): Option[Handshake] = {
-    if (bytes.length != 68) None
+    if (bytes.length != 68) return None
     val protocolStrLen = bytes(0)
     val protocolStr = new String(bytes.slice(1, 1 + protocolStrLen), StandardCharsets.ISO_8859_1)
-    if (protocolStr != "BitTorrent protocol") None
+    if (protocolStr != "BitTorrent protocol") return None
     Some(Handshake(
       infoHash = bytes.slice(bytes.length - 40, bytes.length - 20),
       peerId = new String(bytes.slice(bytes.length - 20, bytes.length), StandardCharsets.ISO_8859_1)
